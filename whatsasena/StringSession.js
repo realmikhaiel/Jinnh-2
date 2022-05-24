1f7afff9d50f84e2b7a1ee6e11a0013bedb4e2ef
Mikhaiel_Offical <72667834+realmikhaiel@users.noreply.github.com>
2022-05-24 23:17:20 +0530

    Update StringSession.js

--- whatsasena/StringSession.js
+++ whatsasena/StringSession.js
@@ -1,8 +1,6 @@
 /* Copyright (C) 2020 Yusuf Usta.
-
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
 WhatsAsena - Yusuf Usta
 */
 
@@ -13,7 +11,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('ASENA_SESSION' in process.env && string === undefined) {
+        if ('JIMBROOTAN_SESSION' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
@@ -21,14 +19,14 @@ class StringSession {
             }
         }
         
-        var split = string.split(';;;');
+        var split = string.split(':::');
         if (split.length >= 2) {
             return JSON.parse(Buffer.from(split[split.length - 1], 'base64').toString('utf-8'));
         }
     }
 
     createStringSession(dict) {
-        return 'ASENA;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'Jimbrootan:::' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
